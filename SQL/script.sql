2cfcf9fcb305ac83b5104e06270285c2d0661670
Samuel de Sousa <samuel.s.santos26@aluno.senai.br>
2026-08-10 11:51:11 -0300

    Add files via upload

---
+++ SQL/script.sql
@@ -0,0 +1,34 @@
+CREATE TABLE dados_pessoais (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cargo VARCHAR(100),
+    resumo TEXT
+);
+
+CREATE TABLE contatos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_curriculo INT NOT NULL,
+    email VARCHAR(100),
+    telefone VARCHAR(20),
+    perfil_profissional VARCHAR(150), -- ex: LinkedIn, GitHub
+    FOREIGN KEY (id_curriculo) REFERENCES dados_pessoais(id) ON DELETE CASCADE
+);
+
+CREATE TABLE experiencias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_curriculo INT NOT NULL,
+    empresa VARCHAR(100),
+    funcao VARCHAR(100),
+    periodo VARCHAR(50),
+    descricao TEXT,
+    FOREIGN KEY (id_curriculo) REFERENCES dados_pessoais(id) ON DELETE CASCADE
+);
+
+CREATE TABLE formacao (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_curriculo INT NOT NULL,
+    instituicao VARCHAR(100),
+    curso VARCHAR(100),
+    periodo VARCHAR(50),
+    FOREIGN KEY (id_curriculo) REFERENCES dados_pessoais(id) ON DELETE CASCADE
+);
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+require_once 'crud.php';
+
+$curriculos = readAll($pdo, 'dados_pessoais');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Currículo Digital</title>
+</head>
+<body>
+    <header>
+        <h1>Plataforma de Currículos</h1>
+        <a href="criar.php">+ Criar um novo Currículo</a>
+    </header>
+
+    <main>
+        <?php if (empty($curriculos)): ?>
+
+        <p class="vazio">Nenhum currículo adicionado ainda.</p>
+
+        <?php else: ?>
+
+        <div class="lista-curriculos">
+            <?php foreach ($curriculos as $curriculo): ?>
+                <h1>Currículos</h1>
+            <div class="card">
+                <h2><?php htmlspecialchars($curriculo['nome']) ?></h2>
+
+                <p class="cargo"><?php htmlspecialchars($curriculo['cargo']) ?></p>
+                <p class="resumo"><?php htmlspecialchars($curriculo['resumo']) ?></p>
+            
+
+            <div class="acoes">
+                <a href="visualizar.php?id=<?= $curriculo['id'] ?>" class="btn-ver">Ver</a>
+                <a href="editar.php?id=<?= $curriculo['id'] ?>" class="btn-editar">Editar</a>
+                <a href="delete.php?id=<?= $curriculo['id'] ?>" class="btn-excluir">Excluir</a>
+            
+            </div>
+
+            </div>
+            <?php endforeach; ?>
+        </div>
+        <?php endif; ?>
+    </main>
+</body>
+</html>
