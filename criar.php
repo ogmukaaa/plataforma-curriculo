3938d8281dec180e33c28ddedf27eec2aa6f5831
Samuel De Sousa <samuel.s.santos26@aluno.senai.br>
2026-08-10 19:43:33 -0300

    Inicio pagina de criação

---
+++ criar.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'crud.php'
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id_curriculo = create($pdo, 'dados_pessoais'), [
+        'nome' => $_POST['nome']
+        'cargo' =>
+    ]
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
